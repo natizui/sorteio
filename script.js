180a3a3f2e90d47b6066d2da71440192292da122
natizui <izui.nm@gmail.com>
2018-04-09 05:20:37 -0300

    sorteio funcionando, falta atualizar os arquivos xml

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+
 //primeiro criar um array com todos os documentos
 //cada documento xml contem os nomes das interessadas no sorteio de cada item
 
 //criar a conexão com o arquivo
 if (window.XMLHttpRequest) {
     var conectar = new XMLHttpRequest();
- } else {    // IE 5/6
+} else {    // IE 5/6
     var conectar = new ActiveXObject("Microsoft.XMLHTTP");
  }
-//função que define qual arquivo pegar, faz a solicitação e coloca o arquivo xml em oDocumento...
-function conexao(documento) {
+ //função que define qual arquivo pegar, faz a solicitação e coloca o arquivo xml em oDocumento...
+ function conexao(documento) {
     conectar.open("GET", documento, false);
     conectar.setRequestHeader("Content-Type", "text/xml");
     conectar.send(null);
@@ -36,6 +37,7 @@ function conexao(documento) {
 var todosDocumentos = ["adesivoArquillian.xml", "adesivoAtomic.xml", "adesivoBranco.xml", "adesivoOVirt.xml", "adesivoVermelho.xml", "bottom.xml", "carteira.xml", "sacola.xml"];
 //conectados recebe o resultado da execução da função conexão para cada um dos elementos de todosDocumentos
 var conectados = todosDocumentos.map(conexao);
+console.log("conectados");
 console.log(conectados);
 //conectados é um array com todos os paraSortear (que é um array com os nomes a serem sorteados)
 //cortando conectados tem-se os arrays paraSortear separados 
@@ -43,44 +45,56 @@ console.log(conectados);
 var resultadoFinal = [];
 
 function sortear() {
-    console.log("realizando sorteio");
+    var itemASerSorteado = document.getElementById("brinde").value;
+    var quantidadeDoItem = document.getElementById("quantidade").value;
+    console.log("item: " + itemASerSorteado);
+    console.log("quantidade: "+ quantidadeDoItem);
+    // console.log("realizando sorteio");
     for (let i = 0; i < conectados.length; i++) {
         var paraSortear = conectados[i];
-        console.log(paraSortear[0]);
+        console.log("paraSortear: "+ paraSortear[0]);
         if(paraSortear[0] == itemASerSorteado) {
             var indiceMax = paraSortear.length-2;
             for  (let i = 0; i < quantidadeDoItem; i++) {
                 //sortear um número aleatório
-                console.log('sorteio '+i);
+                console.log('sorteio: '+i);
                 function sortearUmNumero() {
                     return (Math.floor((Math.random() * indiceMax) + 1));
                 }
                 var contador = 0;
                 do {
                     var resultadoProvisorio = paraSortear[sortearUmNumero()];
-                    console.log("resultadoProvisorio = " + resultadoProvisorio);
+                    console.log("resultadoProvisorio: " + resultadoProvisorio);
                     function checarSeNaoEhRepetido() {
-                        console.log(resultadoFinal);
                         for (let c = 0; c <= i ; c++){
                             if (resultadoProvisorio != resultadoFinal[c]){
-                               var x = true;
+                                var x = true;
                             }
                             else {
                                 return false;
                             }
                         }
+                        console.log("resultadoFinal: " + resultadoFinal);
                         return true;
                     }
                     contador++;
                 } while (!checarSeNaoEhRepetido() && contador<200); 
                 resultadoFinal.push(resultadoProvisorio);
-                console.log(resultadoFinal);
+                console.log("resultadoFinal final: " + resultadoFinal);
+                console.log(i);
+                while (i === quantidadeDoItem-1) {
+                    console.log("while imprimir");
+                    imprimir();                    
+                    i++;
+                }
             }
         }
     }
 }
-var itemASerSorteado = "bottom.xml";
-var quantidadeDoItem = 2;
-sortear();
-document.getElementById("sorteadas").innerHTML = resultadoFinal;
 
+function imprimir() {
+    let imprimirResultado = resultadoFinal.join(" \n "); 
+    console.log("imprim: " + imprimirResultado);
+    window.alert(imprimirResultado);
+    
+}
